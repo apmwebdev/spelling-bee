eae05ce3020b9ec91c7ecc54263e8ce82f12183c
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-06-20 19:03:56 -0700

    Add basic API functionality

--- bee-react/src/App.tsx
+++ bee-react/src/App.tsx
@@ -1,9 +1,27 @@
-import './App.css'
+import './App.css';
+import axios from "axios";
+import Words from "./components/words.tsx";
+import { useEffect, useState } from 'react';
+
+const API_URL = "http://localhost:3000/api/v1/words";
+
+function getAPIData() {
+  return axios.get(API_URL).then(response => response.data);
+}
 
 function App() {
-
+  const [words, setWords] = useState([]);
+  useEffect(() => {
+    let mounted = true;
+    getAPIData().then(items => {
+      if (mounted) {
+        setWords(items);
+      }
+    });
+    return () => {mounted = false};
+  }, []);
   return (
-    <h1>Hello</h1>
+    <Words words={words} />
   )
 }
 
