23166010851034bf7a084f21c9845e144f86fb66
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-06-27 13:09:09 -0700

    Separate puzzle components properly

--- bee-redux/src/features/puzzle/Puzzle.tsx
+++ bee-redux/src/features/puzzle/Puzzle.tsx
@@ -1,31 +1,7 @@
-import { FormEvent, useState } from "react"
-
-import { useAppSelector, useAppDispatch } from "../../app/hooks"
-import { fetchAsync, selectPuzzle } from "./puzzleSlice"
 import PuzzleControls from "./PuzzleControls"
 
 export function Puzzle() {
-  const data = useAppSelector(selectPuzzle)
-  const dispatch = useAppDispatch()
-
-  const displayData = () => {
-    let retVal
-    if (!data) {
-      retVal = "blank"
-    } else {
-      retVal = (
-        <PuzzleControls
-          centerLetter={data.centerLetter}
-          otherLetters={data.outerLetters}
-        />
-      )
-    }
-    return retVal
-  }
-
   return (
-    <div className="hive-container">
-      {displayData()}
-    </div>
+    <PuzzleControls />
   )
 }
--- bee-redux/src/features/puzzle/PuzzleControls.tsx
+++ bee-redux/src/features/puzzle/PuzzleControls.tsx
@@ -1,20 +1,24 @@
 import React, { FC } from "react"
 import LetterCell from "./LetterCell"
+import { useAppSelector } from "../../app/hooks"
+import { selectPuzzle } from "./puzzleSlice"
 
-interface PuzzleProps {
-  centerLetter: string
-  otherLetters: Array<string>
+const PuzzleControls: FC = () => {
+  const puzzle = useAppSelector(selectPuzzle)
+  let contents
+  if (!puzzle) {
+    contents = <div className="hive">No content</div>
+  } else {
+    contents = (
+      <div className="hive">
+        <LetterCell letter={puzzle.centerLetter} isCenter={true} />
+        {puzzle.outerLetters.map((letter) => {
+          return <LetterCell key={letter} letter={letter} isCenter={false} />
+        })}
+      </div>
+    )
+  }
+  return <div className="hive-container">{contents}</div>
 }
 
-const Puzzle: FC<PuzzleProps> = (props) => {
-  return (
-    <div className="hive">
-      <LetterCell letter={props.centerLetter} isCenter={true} />
-      {props.otherLetters.map((letter) => {
-        return <LetterCell key={letter} letter={letter} isCenter={false} />
-      })}
-    </div>
-  )
-}
-
-export default Puzzle
+export default PuzzleControls
