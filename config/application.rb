b9d08e46ddb833f6d436e7f0ca24aff2b1e06046
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-07-18 00:08:40 -0700

    Change NYT Puzzle migration to save JSON

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module SpellingBee
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.active_record.schema_format = :sql
   end
 end
--- db/migrate/20230718061549_create_nyt_puzzles.rb
+++ db/migrate/20230718061549_create_nyt_puzzles.rb
@@ -2,7 +2,7 @@ class CreateNytPuzzles < ActiveRecord::Migration[7.0]
   def change
     create_table :nyt_puzzles do |t|
       t.integer :nyt_id
-      t.jsonb :json_data
+      t.column :json_data, :jsonb
 
       t.timestamps
     end
