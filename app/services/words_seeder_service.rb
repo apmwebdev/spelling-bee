9535421d89ed8eeff687b661a942052b4090159e
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-07-17 22:39:28 -0700

    Add word seeder service

---
+++ app/services/words_seeder_service.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module WordsSeederService
+  def self.seed_from_file
+    File.foreach("db/words_alpha.txt") do |line|
+      Word.create({ text: line.strip })
+    end
+  end
+
+  def self.test_seed
+    first_ten = File.foreach("db/words_alpha.txt").first(10)
+    first_ten.each do |word|
+      Word.create({ text: word.strip })
+    end
+  end
+end
