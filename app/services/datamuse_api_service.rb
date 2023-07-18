63443c4c4a623339fbb14bee80d30d0792476271
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-07-17 20:45:00 -0700

    Add Datamuse API service object

---
+++ app/services/datamuse_api_service.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+require "open-uri"
+require "json"
+
+module DatamuseAPIService
+  def self.get_word_data(word)
+    return_object = {
+      text: word,
+      frequency: 0,
+      definitions: []
+    }
+    response = URI.open("https://api.datamuse.com/words?sp=#{word}&qe=sp&md=fd&max=1").read
+    json = JSON.parse(response)
+    frequency_prop = json[0]["tags"].find do |tag|
+      tag[0..1] == "f:"
+    end
+    return_object[:frequency] = frequency_prop[2..].to_f
+    return_object[:definitions] = json[0]["defs"]
+    p return_object
+  end
+  get_word_data("orotund")
+end
