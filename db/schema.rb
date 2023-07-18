5c2c39511d3015c9354bc615fbb0cc9e89e3e68e
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-07-17 19:18:30 -0700

    Drop words table to recreate it with word as p key

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_230237) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_18_021604) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "words", force: :cascade do |t|
-    t.string "word"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
