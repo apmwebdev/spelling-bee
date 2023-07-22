d35c26e8b28cdb10ccaad621233c19c3b53f3be3
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-07-22 00:50:20 -0700

    Fix semicolons in this file

--- bee-redux/src/features/guesses/GuessAlerts.tsx
+++ bee-redux/src/features/guesses/GuessAlerts.tsx
@@ -1,23 +1,21 @@
 export interface GuessAlertsProps {
-  messages: string[]
-  messagesType: "answer" | "error" | ""
+  messages: string[];
+  messagesType: "answer" | "error" | "";
 }
 
 export function initialGuessAlerts(): GuessAlertsProps {
   return {
     messages: [],
     messagesType: "",
-  }
+  };
 }
 
 export function GuessAlerts({ messages, messagesType }: GuessAlertsProps) {
   const messageOutput = () => {
-    return messages.join("\n ")
-  }
+    return messages.join("\n ");
+  };
 
   return (
-    <div className="sb-guess-input-alerts-container">
-      {messageOutput()}
-    </div>
-  )
+    <div className="sb-guess-input-alerts-container">{messageOutput()}</div>
+  );
 }
