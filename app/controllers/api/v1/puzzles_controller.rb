bdc7807a6edbe2f0d18fc43a16042fd6dd00dcf4
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-08-06 09:25:52 -0700

    Remove auth requirement for puzzles controller

--- app/controllers/api/v1/puzzles_controller.rb
+++ app/controllers/api/v1/puzzles_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::PuzzlesController < ApplicationController
-  before_action :authenticate_user!
   before_action :set_puzzle, only: %i[ show update destroy ]
   # before_action :set_puzzle_by_date_string, only: :show_by_date
   # before_action :set_puzzle_by_letters, only: :show_by_letters
