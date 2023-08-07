323e87c568175b643b54493132318abd28e7a8cf
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-08-07 16:55:29 -0700

    Refactor colors

--- bee-redux/src/features/puzzle/LetterCell.tsx
+++ bee-redux/src/features/puzzle/LetterCell.tsx
@@ -18,7 +18,6 @@ export default function LetterCell({ letter, isCenter }: LetterCellProps) {
     >
       <polygon
         points="0,60.62177826 35,0 105,0 140,60.62177826 105,121.2435565 35,121.2435565"
-        stroke="#242424"
         strokeWidth="8.75"
       ></polygon>
       <text x="50%" y="50%" dy="0.35em">
