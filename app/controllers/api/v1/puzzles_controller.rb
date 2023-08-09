4e7a71553ccedd37edfe88a48eec49306f760b3a
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-08-08 19:48:39 -0700

    Tighten puzzle identifier validation on back end

--- app/controllers/api/v1/puzzles_controller.rb
+++ app/controllers/api/v1/puzzles_controller.rb
@@ -1,7 +1,5 @@
 class Api::V1::PuzzlesController < ApplicationController
   before_action :set_puzzle, only: %i[ show update destroy ]
-  # before_action :set_puzzle_by_date_string, only: :show_by_date
-  # before_action :set_puzzle_by_letters, only: :show_by_letters
 
   # GET /puzzles
   def index
@@ -18,11 +16,9 @@ class Api::V1::PuzzlesController < ApplicationController
   end
 
   # GET /puzzles/latest
-  # Get the latest puzzle and redirect to :show with tts date string
+  # Get the latest puzzle and redirect to :show with its date string
   def latest
-    puzzle = Puzzle.includes(:answers).last
-    puzzle_date_string = puzzle.date.strftime("%Y_%m_%d")
-    redirect_to action: :show, identifier: puzzle_date_string
+    redirect_to action: :show, identifier: Puzzle.last.id
   end
 
   # POST /puzzles
@@ -51,6 +47,7 @@ class Api::V1::PuzzlesController < ApplicationController
   end
 
   private
+
     # Find puzzle by ID, date string, or letters
     def set_puzzle
       identifier = params[:identifier].to_s
@@ -60,11 +57,14 @@ class Api::V1::PuzzlesController < ApplicationController
       # Month must start with a 0 or 1, then any digit.
       # Day must start with a 0, 1, 2, or 3, then any digit.
       # Months and days must be 0-padded (e.g., January is 01, not 1).
-      date_regex = /^20[1-2]\d_?[0-1]\d_?[0-3]\d$/
+      date_regex = /\A20[1-2]\d_?[0-1]\d_?[0-3]\d\z/
 
       # Letter string format is exactly 7 letters, with any combination of
       # capitals and lowercase.
-      letter_regex = /^[A-Za-z]{7}$/
+      letter_regex = /\A[A-Za-z]{7}\z/
+
+      # ID format is one or more digits.
+      id_regex = /\A\d+\z/
 
       # If the identifier is a date string, delete any underscores from it, then
       # convert it to a date and find the puzzle that way.
@@ -80,7 +80,6 @@ class Api::V1::PuzzlesController < ApplicationController
         capital_index = /[A-Z]/ =~ identifier
         # How many capital letters are there?
         num_of_caps = identifier.count("A-Z")
-
         # If there are no capital letters or multiple capital letters, use the
         # first letter as the center letter.
         if capital_index.nil? || num_of_caps > 1
@@ -88,7 +87,6 @@ class Api::V1::PuzzlesController < ApplicationController
             center_letter: identifier.first.downcase,
             outer_letters: identifier[1..6].downcase.split("").sort
           )
-
           # Otherwise, there is exactly one capital letter, so use that as the
           # center letter.
         else
@@ -99,11 +97,14 @@ class Api::V1::PuzzlesController < ApplicationController
           )
         end
 
-        # Identifier isn't a date or letter string, so it must be an ID.
-      else
+        # If identifier is only digits, check if there's a puzzle with that ID.
+      elsif identifier.match(id_regex)
         @puzzle = Puzzle.includes(:answers, :words).find(identifier.to_i)
+
+        # If identifier doesn't match any known format, return a 400.
+      else
+        render json: {}, status: :bad_request
       end
-      @puzzle.set_derived_fields
     end
 
     # Only allow a list of trusted parameters through.
