c72c01ac9545b18ab92fa9a4be3ddb45e5c87f0d
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-09-18 15:31:05 -0700

    Fix typo in Status component name

--- bee-redux/src/features/status/components/Status.tsx
+++ bee-redux/src/features/status/components/Status.tsx
@@ -3,7 +3,7 @@ import { WordLists } from "@/features/wordLists";
 import { AttemptControls } from "@/features/guesses";
 import "@/styles/status.scss";
 
-export function Statuses() {
+export function Status() {
   return (
     <div className="Status">
       <AttemptControls />
