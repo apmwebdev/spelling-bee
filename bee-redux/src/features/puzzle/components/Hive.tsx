bdffaf5816b306864e3d144b7a5402d5e33b83a7
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-09-19 22:56:02 -0700

    Add letter shuffle button

--- bee-redux/src/features/puzzle/components/Hive.tsx
+++ bee-redux/src/features/puzzle/components/Hive.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import LetterCell from "./LetterCell";
 import { useAppSelector } from "@/app/hooks";
-import { selectPuzzle } from "@/features/puzzle";
+import {
+  selectCenterLetter,
+  selectShuffledOuterLetters,
+} from "@/features/puzzle";
 import uniqid from "uniqid";
 
 export function Hive() {
-  const puzzle = useAppSelector(selectPuzzle);
+  const centerLetter = useAppSelector(selectCenterLetter);
+  const outerLetters = useAppSelector(selectShuffledOuterLetters);
   return (
     <div className="HiveContainer">
       <div className="Hive">
-        <LetterCell letter={puzzle.centerLetter} isCenter={true} />
-        {puzzle.outerLetters.map((letter) => {
+        <LetterCell letter={centerLetter} isCenter={true} />
+        {outerLetters.map((letter) => {
           return <LetterCell key={uniqid()} letter={letter} isCenter={false} />;
         })}
       </div>
