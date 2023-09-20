f4c65252ccaef0a4097ef30271ef05af56b94ca1
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-09-20 04:32:48 -0700

    Refactor colors

--- bee-redux/src/features/status/components/Status.tsx
+++ bee-redux/src/features/status/components/Status.tsx
@@ -1,14 +1,14 @@
 import { Progress } from "./Progress";
 import { WordLists } from "@/features/wordLists";
-import { AttemptControls } from "@/features/guesses";
 import "@/styles/status.scss";
+import { AttemptControls } from "@/features/guesses";
 
 export function Status() {
   return (
     <div className="Status">
-      <AttemptControls />
       <Progress />
       <WordLists />
+      <AttemptControls />
     </div>
   );
 }
