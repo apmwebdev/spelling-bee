06acd907a4199155c0e97f0f8c1112854ae68a6f
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-09-24 09:47:16 -0700

    Make getUserBaseData work for guest users
    Make the query work for guest users
    Persist data for child queries to localStorage
    Load data for child queries from localStorage

--- bee-redux/src/features/api/util/index.ts
+++ bee-redux/src/features/api/util/index.ts
@@ -1,4 +1,6 @@
 export * from "./debouncer";
+export * from "./persister";
+
 export const toSnakeCase = (str: string) =>
   str.replace(/[A-Z]/g, (letter) => `_${letter.toLowerCase()}`);
 
--- bee-redux/src/features/userData/api/userDataApiSlice.ts
+++ bee-redux/src/features/userData/api/userDataApiSlice.ts
@@ -1,4 +1,4 @@
-import { apiSlice } from "@/features/api";
+import { apiSlice, persister } from "@/features/api";
 import { hintApiSlice } from "@/features/hints";
 import {
   UserBaseData,
@@ -21,84 +21,128 @@ const mapUserPrefsFormDataToUserPrefs = (
 
 export const userDataApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    /** Triggers getUserHintProfile if currentHintProfile is a UserHintProfile.*/
-    getUserPrefs: builder.query<UserPrefsData, void>({
-      query: () => ({
+    getUserPrefs: builder.query<UserPrefsData, boolean>({
+      query: (_shouldCache) => ({
         url: "/user_prefs",
       }),
+      onQueryStarted: async (
+        shouldCache,
+        { queryFulfilled, getCacheEntry },
+      ) => {
+        if (shouldCache) {
+          await queryFulfilled;
+          const cacheEntry = getCacheEntry();
+          if (cacheEntry.isSuccess && cacheEntry.data) {
+            persister.save("userPrefs", cacheEntry.data);
+          }
+        }
+      },
     }),
     /**
      * Needs to update getUserPrefs endpoint with response.
      * Can use optimistic updates
      */
-    updateUserPrefs: builder.mutation<UserPrefsData, UserPrefsFormData>({
-      query: (formData) => ({
-        url: "/user_prefs",
-        method: "PATCH",
-        body: formData,
-      }),
-      onQueryStarted: (formData, { dispatch, queryFulfilled }) => {
-        const patchResult = dispatch(
+    updateUserPrefs: builder.mutation<boolean, UserPrefsFormData>({
+      queryFn: async (formData, api, _opts, baseQuery) => {
+        api.dispatch(
           userDataApiSlice.util.updateQueryData(
             "getUserPrefs",
-            undefined,
+            true,
             (draft) => {
               mapUserPrefsFormDataToUserPrefs(formData, draft);
             },
           ),
         );
-        queryFulfilled.catch(patchResult.undo);
+        const state = api.getState() as RootState;
+        if (state.auth.user) {
+          await baseQuery({
+            url: "/user_prefs",
+            method: "PATCH",
+            body: formData,
+          });
+        }
+        return { data: true };
       },
     }),
     /**
      * For initial page load. Can fetch before puzzle loads. Combines
-     * - getPrefs
      * - getHintProfiles from hintApiSlice
      * - getCurrentHintProfile, if applicable, from hintApiSlice
+     * - getPrefs
      * Those 3 endpoints need to be updated with the returned data
+     * The latter 2 can be loaded from localStorage
      */
     getUserBaseData: builder.query<UserBaseData, void>({
-      query: () => ({
-        url: "/user_base_data",
-      }),
-      providesTags: ["User"],
-      onQueryStarted: async (
-        _arg,
-        { dispatch, getCacheEntry, queryFulfilled },
-      ) => {
-        await queryFulfilled;
-        const cacheEntry = getCacheEntry();
-        if (cacheEntry.isSuccess && cacheEntry.data) {
-          const { data } = cacheEntry;
+      queryFn: async (_args, { dispatch }, _opts, baseQuery) => {
+        //Load prefs and current profile from localStorage
+        const storedPrefs = persister.load("userPrefs");
+        if (storedPrefs) {
           dispatch(
             userDataApiSlice.util.upsertQueryData(
               "getUserPrefs",
-              undefined,
-              data.prefs,
-            ),
-          );
-          dispatch(
-            hintApiSlice.util.upsertQueryData(
-              "getHintProfiles",
-              undefined,
-              data.hintProfiles,
-            ),
-          );
-          dispatch(
-            hintApiSlice.util.upsertQueryData(
-              "getCurrentHintProfile",
-              undefined,
-              data.currentHintProfile,
+              false,
+              storedPrefs.parsed,
             ),
           );
         }
+        const storedCurrentProfile = persister.load("currentHintProfile");
+        if (storedCurrentProfile) {
+          dispatch(
+            hintApiSlice.util.upsertQueryData(
+              "getCurrentHintProfile",
+              false,
+              storedCurrentProfile.parsed,
+            ),
+          );
+        }
+
+        //Get the response from the server
+        const response = await baseQuery({
+          url: "/user_base_data",
+        });
+        const data = response.data as UserBaseData;
+
+        //Always upsert the default profiles
+        dispatch(
+          hintApiSlice.util.upsertQueryData(
+            "getHintProfiles",
+            undefined,
+            data.hintProfiles,
+          ),
+        );
+        // If returned prefs are different from stored prefs, go with returned
+        // prefs
+        if (
+          data.isLoggedIn &&
+          JSON.stringify(data.prefs) !== storedPrefs?.saved
+        ) {
+          dispatch(
+            userDataApiSlice.util.upsertQueryData(
+              "getUserPrefs",
+              true,
+              data.prefs,
+            ),
+          );
+        }
+        //If returned currentHintProfile is different from stored, overwrite
+        // stored with returned
+        dispatch(
+          hintApiSlice.util.upsertQueryData(
+            "getCurrentHintProfile",
+            true,
+            data.currentHintProfile,
+          ),
+        );
+
+        return { data };
       },
+      providesTags: ["User"],
     }),
     /**
      * For after the puzzle loads, as it requires the puzzle ID. Combines
      * - getCurrentAttempts
      * - getSearches
-     * - getDefinitions
+     * - getDefinitions (not implemented yet)
      */
     getUserPuzzleData: builder.query<UserPuzzleData, number>({
       query: (puzzleId: number) => ({
@@ -106,7 +150,7 @@ export const userDataApiSlice = apiSlice.injectEndpoints({
       }),
       providesTags: ["User"],
       onQueryStarted: async (
-        puzzleId,
+        _puzzleId,
         { dispatch, getState, getCacheEntry, queryFulfilled },
       ) => {
         await queryFulfilled;
