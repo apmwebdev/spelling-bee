077ea8c9aa3618b859b55467a0dea5cfb79a0620
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-10-16 11:10:36 -0700

    Change auth 'More Actions' to be dropdown

--- bee-redux/src/components/radix-ui/radix-collapsible.tsx
+++ bee-redux/src/components/radix-ui/radix-collapsible.tsx
@@ -1,19 +1,52 @@
 import * as Collapsible from "@radix-ui/react-collapsible";
-import { CollapsibleTriggerProps } from "@radix-ui/react-collapsible";
+import {
+  CollapsibleContentProps,
+  CollapsibleProps,
+  CollapsibleTriggerProps,
+} from "@radix-ui/react-collapsible";
 import { RefAttributes } from "react";
 import { HeaderDisclosureWidget } from "@/components/HeaderDisclosureWidget";
+import { composeClasses } from "@/util";
+import classNames from "classnames/dedupe";
 import IntrinsicAttributes = React.JSX.IntrinsicAttributes;
 
-export { Root, Content } from "@radix-ui/react-collapsible";
+export const Root = (
+  props: IntrinsicAttributes & CollapsibleProps & RefAttributes<HTMLDivElement>,
+) => (
+  <Collapsible.Root
+    {...props}
+    className={composeClasses("CollapsibleRoot", props.className)}
+  >
+    {props.children}
+  </Collapsible.Root>
+);
 
 export const Trigger = (
   props: IntrinsicAttributes &
     CollapsibleTriggerProps &
     RefAttributes<HTMLButtonElement> & { title: string },
-) => (
-  <Collapsible.Trigger asChild>
-    <button {...props}>
-      <HeaderDisclosureWidget title={props.title} />
-    </button>
-  </Collapsible.Trigger>
-);
+) => {
+  const { title, className, ...rest } = props;
+  return (
+    <Collapsible.Trigger asChild>
+      <button {...rest} className={classNames("CollapsibleTrigger", className)}>
+        <HeaderDisclosureWidget title={title} />
+      </button>
+    </Collapsible.Trigger>
+  );
+};
+
+export const Content = (
+  props: IntrinsicAttributes &
+    CollapsibleContentProps &
+    RefAttributes<HTMLDivElement>,
+) => {
+  return (
+    <Collapsible.Content
+      {...props}
+      className={classNames("CollapsibleContent", props.className)}
+    >
+      {props.children}
+    </Collapsible.Content>
+  );
+};
