3c122d56c1aa68d16d2d52940fabf06d92fddad4
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-10-18 04:44:43 -0700

    Fix Capistrano .env config

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,3 +44,7 @@ set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+invoke 'dotenv:read'
+invoke 'dotenv:setup'
+set :env_file, ".env.#{stage}"
