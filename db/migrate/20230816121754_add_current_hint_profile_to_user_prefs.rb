5e8613bf9356be3cadd546b51c8e4da886599c72
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-10-18 06:24:54 -0700

    Really fix user_prefs migration

--- db/migrate/20230816121754_add_current_hint_profile_to_user_prefs.rb
+++ db/migrate/20230816121754_add_current_hint_profile_to_user_prefs.rb
@@ -1,6 +1,5 @@
 class AddCurrentHintProfileToUserPrefs < ActiveRecord::Migration[7.0]
   def change
-    add_reference :user_prefs, :current_hint_profile, polymorphic: true,
-      if_not_exists: true
+    # add_reference :user_prefs, :current_hint_profile, polymorphic: true
   end
 end
