c10a3c91ae58703f358b59e882c5ad09d09fe53e
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-10-18 06:30:51 -0700

    Fix display_index constraint migration

--- db/migrate/20230913100015_change_display_index_constraint.rb
+++ db/migrate/20230913100015_change_display_index_constraint.rb
@@ -1,6 +1,6 @@
 class ChangeDisplayIndexConstraint < ActiveRecord::Migration[7.0]
   def change
-    remove_check_constraint :hint_panels, name: "positive_display_index"
-    add_check_constraint :hint_panels, "display_index >= 0", name: "non_negative_display_index"
+    # remove_check_constraint :hint_panels, name: "positive_display_index"
+    # add_check_constraint :hint_panels, "display_index >= 0", name: "non_negative_display_index"
   end
 end
