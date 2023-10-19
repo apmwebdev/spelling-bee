39b0caf19ffb88bb46c2dbb8da3abf7bd5ab6041
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-10-19 04:39:08 -0700

    Fix puma

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -48,7 +48,7 @@ namespace :puma do
   task :start do
     on roles(fetch(:puma_role)) do
       within current_path do
-        execute :bundle, :exec, :puma, "-C /home/deploy/ssb/current/config/puma.rb -e #{fetch(:puma_env)} -d"
+        execute "bundle exec puma -C /home/deploy/ssb/current/config/puma.rb -e production -d"
       end
     end
   end
