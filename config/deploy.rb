add3f666f828de13a72ef736ce237944d5d2d49f
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-10-19 01:10:04 -0700

    Tweak word_alpha task

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,9 +26,11 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets',
 # it to the server once, and keep it out of git.
 desc "Ensure word_alpha.txt.zip exists"
 task :ensure_words_alpha_exists do
-  unless test("[ -f #{shared_path}/db/seeds/words_alpha.txt.zip ]")
-    upload! "db/seeds/words_alpha.txt.zip", "#{shared_path}/db/seeds/",
-      recursive: false
+  on roles(:app) do
+    unless test("[ -f #{shared_path}/db/seeds/words_alpha.txt.zip ]")
+      upload! "db/seeds/words_alpha.txt.zip", "#{shared_path}/db/seeds/words_alpha.txt.zip",
+        recursive: false
+    end
   end
 end
 before "deploy:symlink:linked_files", "ensure_words_alpha_exists"
