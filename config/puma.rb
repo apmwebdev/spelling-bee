153f56c1307951428451dbe1380311b43a0b203f
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-10-19 03:43:23 -0700

    Fix puma config

--- config/puma.rb
+++ config/puma.rb
@@ -6,7 +6,11 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 port ENV.fetch("PORT") { 3000 }
 
-environment ENV.fetch("RAILS_ENV") { "development" }
+if Rails.env.development?
+  environment ENV.fetch("RAILS_ENV") { "development" }
+else
+  environment ENV.fetch("RAILS_ENV") { "production" }
+end
 
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
