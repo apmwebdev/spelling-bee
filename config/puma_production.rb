f6b31824d00ed79ce476ed4fd70e45db9e639f73
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-10-22 21:22:59 -0700

    Re-enable puma-daemon for production

--- config/puma_production.rb
+++ config/puma_production.rb
@@ -1,4 +1,4 @@
-# require "puma/daemon"
+require "puma/daemon"
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
@@ -9,13 +9,11 @@ port ENV.fetch("PORT") { 3000 }
 
 environment ENV.fetch("RAILS_ENV") { "production" }
 
-# pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
-
 stdout_redirect '/home/deploy/ssb/shared/log/puma_access.log', '/home/deploy/ssb/shared/log/puma_error.log', true
 directory '/home/deploy/ssb/current'
 bind "unix:///home/deploy/ssb/shared/tmp/sockets/puma.sock"
 pidfile "/home/deploy/ssb/shared/tmp/pids/puma.pid"
 state_path "/home/deploy/ssb/shared/tmp/pids/puma.state"
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 plugin :tmp_restart
-# daemonize
+daemonize
