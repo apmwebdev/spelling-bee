350d902927a23a3d1114c4c8e7bbfe77e245b08e
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-10-23 19:28:25 -0700

    Small configuration tweaks

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     if Rails.env.development?
       origins ENV["CORS_DEV_ORIGIN"], ENV["CORS_PREVIEW_ORIGIN"]
     else
-      origins ENV["CORS_FE_ORIGIN"], "http://localhost", "http://127.0.0.1"
+      origins ENV["CORS_FE_ORIGIN"]
     end
 
     resource "*",
