c8cf743799650e1944e8cb3d575cf44448f5d1ab
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-10-24 22:51:13 -0700

    Add validation for NytScraperService

--- app/services/nyt_scraper_service.rb
+++ app/services/nyt_scraper_service.rb
@@ -2,70 +2,83 @@
 require "open-uri"
 require "nokogiri"
 require "json"
+require "logger"
 
-module NytScraperService
-  def self.write_log(to_log)
-    unless File.exist?("log/nyt_scraper_log.txt")
-      File.new("log/nyt_scraper_log.txt", "a").close
+class NytScraperService
+  class NytScraperLogger < Logger
+    def initialize
+      super("log/nyt_scraper.log", "weekly")
+      @formatter = proc do |severity, datetime, _progname, msg|
+        timestamp = datetime.strftime("%Y-%m-%d %H:%M:%S")
+        "#{severity} #{timestamp} - #{msg}\n"
+      end
     end
-    timestamp = DateTime.now.strftime("%Y-%m-%d %H:%M:%S")
-    File.write("log/nyt_scraper_log.txt", "#{timestamp}: #{to_log}\n", mode: "a")
   end
 
-  def self.get_puzzle_json
+  def initialize
+    @logger = NytScraperLogger.new
+  end
+
+  def get_puzzle_json
     doc = Nokogiri::HTML(URI.open("https://www.nytimes.com/puzzles/spelling-bee"))
     game_data = doc.css("#pz-game-root + script").text
     JSON.parse(game_data.slice(game_data.index("{")..))
   end
 
-  def self.create_puzzle_from_json(puzzle_json)
-    write_log "Checking if puzzle already exists in database..."
-    puzzle_date = Date.parse(puzzle_json["displayDate"])
+  def create_puzzle_from_json(puzzle_json)
+    print_date = puzzle_json["printDate"]
+    @logger.info "Checking DB for #{print_date} puzzle."
+    puzzle_date = Date.parse(print_date)
     if Puzzle.exists?(date: puzzle_date)
-      return write_log "Puzzle already present. Exiting importer."
+      return @logger.info "Puzzle exists."
     end
-    write_log "Puzzle not present. Starting import of puzzle for #{puzzle_json["displayDate"]}"
+    @logger.info "Puzzle not present. Starting import..."
     nyt_puzzle = NytPuzzle.create!({nyt_id: puzzle_json["id"], json_data: puzzle_json})
-    write_log "Created NytPuzzle"
+    @logger.info "Created NytPuzzle."
     puzzle = Puzzle.create!({
       date: puzzle_date,
       center_letter: puzzle_json["centerLetter"],
       outer_letters: puzzle_json["outerLetters"],
       origin: nyt_puzzle,
     })
-    write_log "Created Puzzle. ID = #{puzzle.id}"
+    @logger.info "Created Puzzle: ID = #{puzzle.id}, Date = #{print_date}."
     puzzle_json["answers"].each do |answer|
       word = Word.create_or_find_by({text: answer})
       if word.frequency.nil?
-        write_log "Fetching datamuse data for \"#{answer}\""
+        @logger.info "Fetching datamuse data for \"#{answer}\"."
         datamuse_data = DatamuseApiService.get_word_data(answer)
         word.frequency = datamuse_data[:frequency]
         word.definitions = datamuse_data[:definitions]
         word.save
       else
-        write_log "Datamuse data already exists for \"#{answer}\""
+        @logger.info "Datamuse data already exists for \"#{answer}\"."
       end
       Answer.create!({puzzle: puzzle, word_text: answer})
     end
-    write_log "Finished importing puzzle #{puzzle.id}"
+    @logger.info "Finished importing puzzle #{puzzle.id} for #{print_date}"
   end
 
-  def self.import_latest_puzzle
-    write_log "Importing latest puzzle..."
-    create_puzzle_from_json(get_puzzle_json["today"])
-  end
-
-  def self.seed_all_puzzles_json
-    write_log "Seed Puzzles: Starting..."
-    data = get_puzzle_json
-    days_arr = [*data["pastPuzzles"]["lastWeek"]]
-    days_arr.push(*data["pastPuzzles"]["thisWeek"])
-    days_arr.each_with_index do |puzzle_json|
-      create_puzzle_from_json(puzzle_json)
+  def import_latest_puzzle
+    @logger.info "Importing latest puzzle..."
+    puzzles_json = get_puzzle_json
+    if NytScraperValidator.new(@logger, puzzles_json).today_valid?
+      create_puzzle_from_json(puzzles_json["today"])
     end
   end
 
-  def self.log_test
+  def import_all_puzzles
+    @logger.info "Importing all puzzles..."
+    puzzles_json = get_puzzle_json
+    if NytScraperValidator.new(@logger, puzzles_json).valid?
+      @days_arr = [*puzzles_json["pastPuzzles"]["lastWeek"]]
+      @days_arr.push(*puzzles_json["pastPuzzles"]["thisWeek"])
+      @days_arr.each_with_index do |puzzle_json|
+        create_puzzle_from_json(puzzle_json)
+      end
+    end
+  end
+
+  def log_test
     write_log("test")
     write_log("test 2")
   end
---
+++ app/services/nyt_scraper_validator.rb
@@ -0,0 +1,117 @@
+# frozen_string_literal: true
+
+class NytScraperValidator
+  def initialize(logger, json_data)
+    @logger = logger
+    @json_data = json_data
+  end
+
+  def err_log(to_log)
+    err_msg = "Invalid JSON:"
+    @logger.error "#{err_msg} #{to_log}\n"
+  end
+
+  def valid?
+    unless @json_data.is_a?(Hash)
+      err_log "JSON data is not a hash."
+      return false
+    end
+
+    unless structure_matches?(@json_data)
+      err_log "Invalid JSON structure."
+      return false
+    end
+
+    unless today_valid?
+      err_log "Invalid format for 'today'."
+      return false
+    end
+
+    unless valid_puzzles?(@json_data["pastPuzzles"]["thisWeek"])
+      err_log "Invalid format for 'thisWeek'."
+      return false
+    end
+
+    unless valid_puzzles?(@json_data["pastPuzzles"]["lastWeek"])
+      err_log "Invalid format for 'lastWeek'."
+      return false
+    end
+
+    true
+  end
+
+  def structure_matches?(data)
+    unless data.is_a?(Hash)
+      err_log "Structure doesn't match. Data is not a hash."
+      return false
+    end
+    return false unless data.key?("today") && data["today"].is_a?(Hash)
+    return false unless data.key?("pastPuzzles") && data["pastPuzzles"].is_a?(Hash)
+    return false unless data["pastPuzzles"].key?("thisWeek") && data["pastPuzzles"]["thisWeek"].is_a?(Array)
+    return false unless data["pastPuzzles"].key?("lastWeek") && data["pastPuzzles"]["lastWeek"].is_a?(Array)
+
+    true
+  end
+
+  def today_valid?
+    valid_puzzle?(@json_data["today"])
+  end
+
+  def valid_puzzle?(puzzle_data)
+    def bad_puzzle(to_log, puzzle_data)
+      err_log "Invalid puzzle for #{puzzle_data["printDate"]}: #{to_log}"
+    end
+
+    unless puzzle_data.is_a?(Hash)
+      bad_puzzle("Puzzle is not a hash.", puzzle_data)
+      return false
+    end
+    unless puzzle_data.key?("displayDate") && valid_date?(puzzle_data["displayDate"])
+      bad_puzzle("Invalid displayDate.", puzzle_data)
+      return false
+    end
+    unless puzzle_data.key?("centerLetter") && valid_letter?(puzzle_data["centerLetter"])
+      bad_puzzle("Invalid center letter.", puzzle_data)
+      return false
+    end
+    unless puzzle_data.key?("outerLetters") && valid_outer_letters_array?(puzzle_data["outerLetters"])
+      bad_puzzle("Invalid outer letters.", puzzle_data)
+      return false
+    end
+    unless puzzle_data.key?("answers") && valid_answers?(puzzle_data["answers"])
+      bad_puzzle("Invalid answers.", puzzle_data)
+      return false
+    end
+
+    true
+  end
+
+  def valid_puzzles?(puzzles)
+    return false unless puzzles.is_a?(Array)
+    puzzles.all? { |puzzle| valid_puzzle?(puzzle) }
+  end
+
+  def valid_date?(date_string)
+    Date.parse(date_string)
+    true
+  rescue ArgumentError => e
+    @logger.error "Invalid date format: #{date_string}. Error: #{e.message}"
+    false
+  end
+
+  def valid_letter?(letter)
+    letter.match?(/\A[a-zA-Z]\z/)
+  end
+
+  # Outer letters must be an array of exactly 6 letters
+  def valid_outer_letters_array?(letters)
+    return false unless letters.is_a?(Array)
+    return false unless letters.length == 6
+    letters.all? { |letter| valid_letter?(letter) }
+  end
+
+  def valid_answers?(answers)
+    return false unless answers.is_a?(Array)
+    answers.all? { |answer| answer.is_a?(String) }
+  end
+end
