d5947442f59fd67114284d79e8bb7fb68dacf57d
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-10-25 06:31:32 -0700

    Add error boundary to app

--- bee-redux/src/providers/AppProvider.tsx
+++ bee-redux/src/providers/AppProvider.tsx
@@ -1,11 +1,25 @@
 import { GlobalContextProvider } from "@/providers/GlobalContext";
 import { TooltipProvider } from "@radix-ui/react-tooltip";
 import { ReactNode } from "react";
+import { ErrorBoundary, FallbackProps } from "react-error-boundary";
+import { Link } from "@/components/react-router/Link";
+
+const errorFallback = ({ error }: FallbackProps) => (
+  <div>
+    <h1 className="Error_title">Something went wrong</h1>
+    <p className="Error_message mono">{JSON.stringify(error)}</p>
+    <Link to="/">Go to home page</Link>
+  </div>
+);
 
 export function AppProvider({ children }: { children: ReactNode }) {
   return (
-    <GlobalContextProvider>
-      <TooltipProvider delayDuration={900}>{children}</TooltipProvider>
-    </GlobalContextProvider>
+    <ErrorBoundary fallbackRender={errorFallback}>
+      <GlobalContextProvider>
+        <TooltipProvider delayDuration={900}>
+          {children}
+        </TooltipProvider>
+      </GlobalContextProvider>
+    </ErrorBoundary>
   );
 }
