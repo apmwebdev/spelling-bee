9516796d04d1b7ce6c498501371ac7f49b578fc3
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-10-26 03:02:41 -0700

    Add SMTP credentials for mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,19 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "spelling_bee_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: "https://superspellingbee.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:         ENV["SMTP_SERVER"],
+    user_name:       ENV["SMTP_USERNAME"],
+    password:        ENV["SMTP_PASSWORD"],
+    port:            587,
+    authentication:  "plain",
+    enable_starttls: true,
+    open_timeout:    5,
+    read_timeout:    5
+  }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
