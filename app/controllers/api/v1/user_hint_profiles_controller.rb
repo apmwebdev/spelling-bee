ed560aff8027fa035546c4d65aa0c75c56324367
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-11-11 22:03:05 -0800

    Add license file, license info to /app directory

--- app/controllers/api/v1/user_hint_profiles_controller.rb
+++ app/controllers/api/v1/user_hint_profiles_controller.rb
@@ -1,3 +1,13 @@
+# Super Spelling Bee - A vocabulary game with integrated hints
+# Copyright (C) 2023 Austin Miller
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# See the LICENSE file or https://www.gnu.org/licenses/ for more details.
+
 class Api::V1::UserHintProfilesController < AuthRequiredController
   before_action :set_user_hint_profile, only: %i[show update destroy]
   skip_before_action :authenticate_user!, only: :get_all_hint_profiles
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,3 +1,13 @@
+# Super Spelling Bee - A vocabulary game with integrated hints
+# Copyright (C) 2023 Austin Miller
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# See the LICENSE file or https://www.gnu.org/licenses/ for more details.
+
 class Word < ApplicationRecord
   self.primary_key = :text
   has_many :answers, primary_key: :text, foreign_key: :word_text
