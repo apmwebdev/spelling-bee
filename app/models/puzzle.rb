04593a0083d1e148d3c38cb045dc86f49f8317b9
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2023-11-19 16:44:01 -0800

    Add excluded_words cache to db for performance

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -14,22 +14,34 @@ class Puzzle < ApplicationRecord
   has_many :words, through: :answers
   has_many :user_puzzle_attempts
 
-  attr_accessor :pangrams, :perfect_pangrams, :valid_letters, :excluded_words,
-    :is_latest
+  attr_accessor :pangrams, :perfect_pangrams, :valid_letters, :is_latest
 
   def set_valid_letters
     @valid_letters ||= [center_letter, *outer_letters].sort
+  end
+
+  def create_excluded_words_cache
+    return unless excluded_words.nil?
+    set_valid_letters
+    valid_letters_regex = "^[#{valid_letters.join("")}]+$"
+    excluded_words_array = Word
+      .where("CHAR_LENGTH(text) >= 4")
+      .where("text ~ ?", valid_letters_regex)
+      .where("text LIKE ?", "%#{center_letter}%")
+      .where.not(text: words.select(:text))
+      .pluck(:text)
+    update!(excluded_words: excluded_words_array)
   end
 
   def set_pangrams
     @pangrams ||= []
     return unless @pangrams.empty?
     set_valid_letters
-    potential_pangrams = Answer.where(puzzle_id: id).where("length(word_text) >= 7")
-    potential_pangrams.each do |pan|
-      @pangrams.push(pan.word_text) if Set.new(pan.word_text.chars).length == 7
-    end
-    @pangrams
+    @pangrams = Answer
+      .where(puzzle_id: id)
+      .where("length(word_text) >= 7")
+      .where(@valid_letters.map { |letter| "word_text LIKE '%#{letter}%'" }.join(" AND "))
+      .pluck(:word_text)
   end
 
   def set_perfect_pangrams
@@ -43,21 +55,6 @@ class Puzzle < ApplicationRecord
     @perfect_pangrams
   end
 
-  def set_excluded_words
-    set_valid_letters
-    @excluded_words ||= []
-    return unless @excluded_words.empty?
-    Word
-      .where("CHAR_LENGTH(text) >= 4")
-      .where("text ~ '^[#{@valid_letters.join("")}]+$'")
-      .where("text LIKE '%#{center_letter}%'")
-      .find_each do |checked_word|
-      unless words.include?(checked_word)
-        @excluded_words.push(checked_word.text)
-      end
-    end
-  end
-
   def set_is_latest
     @is_latest = Puzzle.last === self
   end
@@ -66,7 +63,6 @@ class Puzzle < ApplicationRecord
     set_valid_letters
     set_pangrams
     set_perfect_pangrams
-    set_excluded_words
     set_is_latest
   end
 
@@ -83,7 +79,7 @@ class Puzzle < ApplicationRecord
       answers: answers.map do |answer|
         answer.to_front_end
       end.sort_by { |answer| answer[:word] },
-      excludedWords: @excluded_words,
+      excludedWords: excluded_words,
       isLatest: @is_latest
     }
   end
