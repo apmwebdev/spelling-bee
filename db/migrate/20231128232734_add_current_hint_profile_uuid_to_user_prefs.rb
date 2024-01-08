8394f4bb87577eaff9323ebfb41583c6ec5e45a1
Austin Miller <30607075+apmwebdev@users.noreply.github.com>
2024-01-07 21:04:53 -0800

    Fix current_hint_profile migration

--- db/migrate/20231128232734_add_current_hint_profile_uuid_to_user_prefs.rb
+++ db/migrate/20231128232734_add_current_hint_profile_uuid_to_user_prefs.rb
@@ -3,7 +3,18 @@ class AddCurrentHintProfileUuidToUserPrefs < ActiveRecord::Migration[7.0]
     add_column :user_prefs, :current_hint_profile_uuid, :uuid
     UserPref.reset_column_information
     UserPref.find_each do |pref|
-      pref.current_hint_profile_uuid = pref.current_hint_profile.uuid
+      if pref.current_hint_profile
+        pref.current_hint_profile_uuid = pref.current_hint_profile.uuid
+        pref.save!
+      elsif pref.current_hint_profile_type == "DefaultHintProfile"
+        profile = DefaultHintProfile.find(pref.current_hint_profile_id)
+        pref.current_hint_profile_uuid = profile.uuid
+        pref.save!
+      elsif pref.current_hint_profile_type == "UserHintProfile"
+        profile = UserHintProfile.find(pref.current_hint_profile_id)
+        pref.current_hint_profile_uuid = profile.uuid
+        pref.save!
+      end
     end
   end
 
